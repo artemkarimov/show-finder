c75a17eed1035544adf7fb01212515adbf06c76d
artemkarimov <tymwrk1@gmail.com>
2021-12-09 23:58:55 +0200

    Client: add layout to the application

---
+++ client/components/layout/index.tsx
@@ -0,0 +1,14 @@
+import type { FunctionComponent } from 'react';
+
+import Header from './header';
+
+const Layout: FunctionComponent = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app';
 
+import Layout from '../components/layout';
 import '../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default MyApp;
