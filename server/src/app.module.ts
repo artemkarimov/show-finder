394437442456945a70a00db1e9331dfaa9e02647
artemkarimov <tymwrk1@gmail.com>
2021-12-11 21:47:23 +0200

    Server: create shows module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ShowsModule } from './shows/shows.module';
 
 @Module({
   controllers: [AppController],
+  imports: [ShowsModule],
 })
 export class AppModule {}
