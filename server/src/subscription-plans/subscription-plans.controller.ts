a474c708e9249dd80265e221d098c39544e6da55
artemkarimov <tymwrk1@gmail.com>
2021-12-12 17:42:10 +0200

    Server: add method for adding subscription plans to the database

--- server/src/subscription-plans/subscription-plans.controller.ts
+++ server/src/subscription-plans/subscription-plans.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AddSubscriptionPlanDto } from './dtos/add-subsription-plan.dto';
+
+import { SubscriptionPlansService } from './subscription-plans.service';
 
 @Controller('subscription-plans')
-export class SubscriptionPlansController {}
+export class SubscriptionPlansController {
+  constructor(private readonly service: SubscriptionPlansService) {}
+
+  @Post()
+  async addSubscriptionPlans(@Body() body: AddSubscriptionPlanDto[]) {
+    return this.service.create(body)
+  }
+}
--- server/src/subscription-plans/subscription-plans.service.ts
+++ server/src/subscription-plans/subscription-plans.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { SubscriptionPlan } from './entities/subscription-plan.entity';
+import { AddSubscriptionPlanDto } from './dtos/add-subsription-plan.dto';
 
 @Injectable()
-export class SubscriptionPlansService {}
+export class SubscriptionPlansService {
+  constructor(
+    @InjectRepository(SubscriptionPlan)
+    private readonly repository: Repository<SubscriptionPlan>,
+  ) {}
+
+  async create(subscriptionPlanDtos: AddSubscriptionPlanDto[]) {
+    for (const subscriptionPlanDto of subscriptionPlanDtos) {
+      const subscriptionPlan = this.repository.create({
+        ...subscriptionPlanDto,
+      });
+      await this.repository.save(subscriptionPlan);
+    }
+  }
+}
