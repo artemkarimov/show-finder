956b38fb19a977aee0d5d675c8d28d232a8dd816
artemkarimov <tymwrk1@gmail.com>
2021-12-13 23:37:42 +0200

    Client: add search for shows handler

--- client/components/inputs/search-input/index.tsx
+++ client/components/inputs/search-input/index.tsx
@@ -1,13 +1,21 @@
-import type { FunctionComponent } from 'react';
+import type { FunctionComponent, ChangeEvent } from 'react';
 
 import styles from './styles.module.scss';
 
 interface Props {
   placeholder: string;
+  searchHandler: (event: ChangeEvent<HTMLInputElement>) => Promise<void>;
 }
 
-const SearchInput: FunctionComponent<Props> = ({ placeholder }) => {
-  return <input type="text" className={styles.input} placeholder={placeholder} />;
+const SearchInput: FunctionComponent<Props> = ({ placeholder, searchHandler }) => {
+  return (
+    <input
+      type="text"
+      className={styles.input}
+      placeholder={placeholder}
+      onChange={searchHandler}
+    />
+  );
 };
 
 export default SearchInput;
--- client/components/search-bar/index.tsx
+++ client/components/search-bar/index.tsx
@@ -1,33 +1,36 @@
-import type { FunctionComponent } from 'react';
+import type { FunctionComponent, ChangeEvent } from 'react';
+import { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 
 import SearchInput from '../inputs/search-input';
+import Show from '../../common/interfaces/show';
 import styles from './styles.module.scss';
 
-interface Props {
-  shows: any[];
-}
-
-const SearchBar: FunctionComponent<Props> = ({ shows }) => {
+const SearchBar: FunctionComponent = () => {
+  const [matchingShows, setMatchingShows] = useState<Show[]>([]);
+  const searchHandler = async (event: ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+  };
   return (
     <>
       <div className={styles['search-input']}>
-        <SearchInput placeholder="Search for..." />
+        <SearchInput placeholder="Search for..." searchHandler={searchHandler} />
         <div className={styles.icon}>
           <SearchIcon />
         </div>
       </div>
       <div className={styles['search-result']}>
-        {shows.map(value => (
-          <a className={styles.item}>
-            <p>
-              {value.title}{' '}
-              <span>
-                ({value.type}, {value.type === 'film' ? value.releaseYear : value.releaseYears})
-              </span>
-            </p>
-          </a>
-        ))}
+        {!!matchingShows.length &&
+          matchingShows.map(value => (
+            <a className={styles.item}>
+              <p>
+                {value.title}{' '}
+                <span>
+                  ({value.type}, {value.type === 'film' ? value.releaseYear : value.releaseYears})
+                </span>
+              </p>
+            </a>
+          ))}
       </div>
     </>
   );
