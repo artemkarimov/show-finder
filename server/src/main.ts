19464c8e821ab224d564f6d25fc72d91b981bec6
artemkarimov <tymwrk1@gmail.com>
2021-12-13 15:27:46 +0200

    Server: change request payload limit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'body-parser';
+
 import { AppModule } from './app.module';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
   await app.listen(3001);
 };
 
