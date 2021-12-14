b503b61d79cd791b1eb8d80034cf72916b9c0f3b
artemkarimov <tymwrk1@gmail.com>
2021-12-14 21:38:44 +0200

    Client: add loading spinner component to show header component

--- client/components/show-header/index.tsx
+++ client/components/show-header/index.tsx
@@ -1,18 +1,33 @@
 import type { FunctionComponent } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
+import LoadingSpinner from '../loading-spinner';
+import loadImage from '../../helpers/image-loader';
+import { FALLBACK_IMAGE_PATH } from '../../constants';
 import styles from './styles.module.scss';
 
 interface Props {
   title: string;
-  image: string;
+  image: string | null;
 }
 
 const ShowHeader: FunctionComponent<Props> = ({ title, image }) => {
+  const [imagePath, setImagePath] = useState<string>('');
+  useEffect(() => {
+    const getImagePath = async () => {
+      if (image) {
+        const url = await loadImage(image);
+        setImagePath(url);
+      } else setImagePath(FALLBACK_IMAGE_PATH);
+    };
+    getImagePath();
+  }, []);
+  if (!imagePath) return <LoadingSpinner />;
   return (
     <header className={styles.header}>
-      <h1>Title</h1>
-      <Image src={image} alt={title} width={200} height={300} />
+      <h1>{title}</h1>
+      <Image src={imagePath} alt={title} width={200} height={300} />
     </header>
   );
 };
