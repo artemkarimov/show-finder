ad60a12ebd8c6102d89b8d8b4d3920635348be3c
artemkarimov <tymwrk1@gmail.com>
2021-12-14 02:28:52 +0200

    Client: add calling api helper function in search handler

--- client/components/search-bar/index.tsx
+++ client/components/search-bar/index.tsx
@@ -1,16 +1,35 @@
 import type { FunctionComponent, ChangeEvent } from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 
 import SearchInput from '../inputs/search-input';
 import Show from '../../common/interfaces/show';
+import { getMatchingShows } from '../../api/api-helper';
 import styles from './styles.module.scss';
 
 const SearchBar: FunctionComponent = () => {
   const [matchingShows, setMatchingShows] = useState<Show[]>([]);
+  const [resultCardHeight, setResultCardHeight] = useState<number>(250);
+  const [noShowsFound, setNoShowsFound] = useState<boolean>(false);
   const searchHandler = async (event: ChangeEvent<HTMLInputElement>) => {
     const input = event.target.value;
+    if (input) {
+      const shows = await getMatchingShows(input);
+      if (shows.length) {
+        setMatchingShows(shows);
+        if (noShowsFound) setNoShowsFound(false);
+      } else setNoShowsFound(true);
+    } else {
+      setMatchingShows([]);
+    }
   };
+  useEffect(() => {
+    if (matchingShows.length === 4) setResultCardHeight(200);
+    else if (matchingShows.length === 3) setResultCardHeight(150);
+    else if (matchingShows.length === 2 || noShowsFound) setResultCardHeight(100);
+    else if (matchingShows.length === 1) setResultCardHeight(50);
+    else if (matchingShows.length !== 5) setResultCardHeight(250);
+  }, [matchingShows, noShowsFound]);
   return (
     <>
       <div className={styles['search-input']}>
@@ -19,10 +38,17 @@ const SearchBar: FunctionComponent = () => {
           <SearchIcon />
         </div>
       </div>
-      <div className={styles['search-result']}>
+      <div
+        className={styles['search-result']}
+        style={{
+          display: matchingShows.length ? 'block' : 'none',
+          height: `${resultCardHeight}px`,
+        }}
+      >
         {!!matchingShows.length &&
+          !noShowsFound &&
           matchingShows.map(value => (
-            <a className={styles.item}>
+            <a className={styles.item} key={value.id}>
               <p>
                 {value.title}{' '}
                 <span>
@@ -31,6 +57,9 @@ const SearchBar: FunctionComponent = () => {
               </p>
             </a>
           ))}
+        {!!matchingShows.length && noShowsFound && (
+          <p className={styles['not-found']}>No Shows Found</p>
+        )}
       </div>
     </>
   );
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
+import SearchBar from '../components/search-bar';
 
 const HomePage: NextPage = () => {
-  return <div></div>;
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default HomePage;
