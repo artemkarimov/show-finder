cdbf1370252d38d5af14b7bc8711434fd10163d8
artemkarimov <tymwrk1@gmail.com>
2021-12-14 16:05:46 +0200

    Client: add function to get blob data to load an image

--- client/api/api-helper.ts
+++ client/api/api-helper.ts
@@ -11,3 +11,19 @@ export const getMatchingShows = async (input: string): Promise<Show[]> => {
   const shows = response.data;
   return shows;
 };
+
+export const getMostSearchedShows = async (limit: number): Promise<Show[]> => {
+  const response: AxiosResponse<Show[]> = await axios.get(
+    `${SERVER_HOST}/shows/most-searched?limit=${limit}`
+  );
+  const shows = response.data;
+  return shows;
+};
+
+export const getBlobData = async (url: string): Promise<Blob> => {
+  const response: AxiosResponse<Blob> = await axios.get(url, {
+    responseType: 'blob',
+  });
+  const blob = response.data;
+  return blob;
+};
