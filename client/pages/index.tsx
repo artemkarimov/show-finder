cb41a4a51580c94917030c47b09e5ab941ecf5a6
artemkarimov <tymwrk1@gmail.com>
2021-12-14 18:06:51 +0200

    Client: add most searched shows static props to the home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,36 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetStaticProps } from 'next';
 
 import SearchBar from '../components/search-bar';
+import MostSearchedShows from '../components/most-searched-shows';
+import Show from '../common/interfaces/show';
+import { getMostSearchedShows } from '../api/api-helper';
 import styles from './styles.module.scss';
 
-const HomePage: NextPage = () => {
+interface Props {
+  mostSearchedShows: Show[];
+}
+
+const HomePage: NextPage<Props> = ({ mostSearchedShows }) => {
+  console.log(mostSearchedShows);
   return (
-    <div className={styles['search-bar']}>
-      <SearchBar />
-    </div>
+    <>
+      <div className={styles['search-bar']}>
+        <SearchBar />
+      </div>
+      <div className={styles['most-searched']}>
+        <MostSearchedShows shows={mostSearchedShows} />
+      </div>
+    </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const mostSearchedShows = await getMostSearchedShows(6);
+  return {
+    props: {
+      mostSearchedShows,
+    },
+  };
 };
 
 export default HomePage;
