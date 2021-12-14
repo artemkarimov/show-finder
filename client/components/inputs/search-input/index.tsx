8d2a7e087c58a1b3379b05cc539dc5298d5272b7
artemkarimov <tymwrk1@gmail.com>
2021-12-14 03:47:14 +0200

    Client: add close icon to the search bar component

--- client/components/inputs/search-input/index.tsx
+++ client/components/inputs/search-input/index.tsx
@@ -3,15 +3,17 @@ import type { FunctionComponent, ChangeEvent } from 'react';
 import styles from './styles.module.scss';
 
 interface Props {
+  value: string;
   placeholder: string;
   searchHandler: (event: ChangeEvent<HTMLInputElement>) => Promise<void>;
 }
 
-const SearchInput: FunctionComponent<Props> = ({ placeholder, searchHandler }) => {
+const SearchInput: FunctionComponent<Props> = ({ placeholder, value, searchHandler }) => {
   return (
     <input
       type="text"
       className={styles.input}
+      value={value}
       placeholder={placeholder}
       onChange={searchHandler}
     />
--- client/components/search-bar/index.tsx
+++ client/components/search-bar/index.tsx
@@ -1,6 +1,7 @@
 import type { FunctionComponent, ChangeEvent } from 'react';
 import { useState, useEffect } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 
 import SearchInput from '../inputs/search-input';
 import Show from '../../common/interfaces/show';
@@ -11,20 +12,8 @@ import styles from './styles.module.scss';
 const SearchBar: FunctionComponent = () => {
   const [matchingShows, setMatchingShows] = useState<Show[]>([]);
   const [resultCardHeight, setResultCardHeight] = useState<number>(250);
+  const [userInput, setUserInput] = useState<string>('');
   const [noShowsFound, setNoShowsFound] = useState<boolean>(false);
-  const searchHandler = async (event: ChangeEvent<HTMLInputElement>) => {
-    const input = event.target.value;
-    if (input) {
-      const shows = await getMatchingShows(input);
-      if (shows.length) {
-        const uniqueShows = getArrayUniqueByKey<Show>(shows, 'title')
-        setMatchingShows(uniqueShows);
-        if (noShowsFound) setNoShowsFound(false);
-      } else setNoShowsFound(true);
-    } else {
-      setMatchingShows([]);
-    }
-  };
   useEffect(() => {
     if (matchingShows.length === 4) setResultCardHeight(200);
     else if (matchingShows.length === 3) setResultCardHeight(150);
@@ -32,18 +21,48 @@ const SearchBar: FunctionComponent = () => {
     else if (matchingShows.length === 1) setResultCardHeight(50);
     else if (matchingShows.length !== 5) setResultCardHeight(250);
   }, [matchingShows, noShowsFound]);
+  const searchHandler = async (event: ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    setUserInput(input);
+    if (input) {
+      const shows = await getMatchingShows(input);
+      if (shows.length) {
+        const uniqueShows = getArrayUniqueByKey<Show>(shows, 'title');
+        setMatchingShows(uniqueShows);
+        if (noShowsFound) setNoShowsFound(false);
+      } else {
+        setNoShowsFound(true);
+      }
+    } else {
+      setMatchingShows([]);
+      if (noShowsFound) setNoShowsFound(false);
+    }
+  };
+  const clearInput = () => {
+    setUserInput('');
+    setMatchingShows([]);
+    if (noShowsFound) setNoShowsFound(false);
+  };
   return (
     <>
       <div className={styles['search-input']}>
-        <SearchInput placeholder="Search for..." searchHandler={searchHandler} />
+        <SearchInput
+          placeholder="Enter a title of the film or the series"
+          value={userInput}
+          searchHandler={searchHandler}
+        />
         <div className={styles.icon}>
-          <SearchIcon />
+          {matchingShows.length ? (
+            <CloseIcon className={styles.close} onClick={clearInput} />
+          ) : (
+            <SearchIcon />
+          )}
         </div>
       </div>
       <div
         className={styles['search-result']}
         style={{
-          display: matchingShows.length ? 'block' : 'none',
+          display: matchingShows.length || noShowsFound ? 'block' : 'none',
           height: `${resultCardHeight}px`,
         }}
       >
@@ -59,9 +78,7 @@ const SearchBar: FunctionComponent = () => {
               </p>
             </a>
           ))}
-        {!!matchingShows.length && noShowsFound && (
-          <p className={styles['not-found']}>No Shows Found</p>
-        )}
+        {noShowsFound && <p className={styles['not-found']}>No Shows Found</p>}
       </div>
     </>
   );
