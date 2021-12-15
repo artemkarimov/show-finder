fd6ed24008f356012b273e11df795edf2cf096c2
artemkarimov <tymwrk1@gmail.com>
2021-12-16 00:56:07 +0200

    Server: add auth service

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
   imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
