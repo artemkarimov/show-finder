352a907897e73902f36fdbb0c397246bfa044f69
artemkarimov <tymwrk1@gmail.com>
2021-12-17 19:20:02 +0200

    Client: add navigation to the header component

--- client/components/header/index.tsx
+++ client/components/header/index.tsx
@@ -1,15 +1,40 @@
 import type { FunctionComponent } from 'react';
 import Link from 'next/link';
+import { useSelector, useDispatch } from 'react-redux';
 
+import { State } from '../../store';
 import StaticRoutes from '../../common/enums/static-routes';
+import { signUserOut } from '../../api/api-helper';
 import styles from './styles.module.scss';
+import { authActions } from '../../store/slices/auth-slice';
 
 const Header: FunctionComponent = () => {
+  const isAuthenticated = useSelector((state: State) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const signoutHandler = async () => {
+    await signUserOut();
+    dispatch(authActions.signOut());
+  };
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <Link href={StaticRoutes.HOME}>ShowFinder</Link>
       </div>
+      <nav>
+        <ul>
+          {!isAuthenticated && (
+            <>
+              <li>
+                <Link href={StaticRoutes.SIGNIN}>Sign in</Link>
+              </li>
+              <li>
+                <Link href={StaticRoutes.SIGNUP}>Sign up</Link>
+              </li>
+            </>
+          )}
+          {isAuthenticated && <li onClick={signoutHandler}>Sign out</li>}
+        </ul>
+      </nav>
     </header>
   );
 };
