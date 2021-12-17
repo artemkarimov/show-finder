26b49569bb042a3a5dda1143f48af671b7ed79ad
artemkarimov <tymwrk1@gmail.com>
2021-12-17 03:44:40 +0200

    Client: add auth api helper functions

--- client/api/api-helper.ts
+++ client/api/api-helper.ts
@@ -2,8 +2,22 @@ import axios, { AxiosResponse } from 'axios';
 
 import Show from '../common/interfaces/show';
 import SubscriptionPlan from '../common/interfaces/subscription-plan';
+import User from '../common/interfaces/user';
 
 const SERVER_HOST = process.env.NEXT_PUBLIC_SERVER_HOST;
+
+interface SignupData {
+  firstName: string;
+  lastName: string;
+  countryId: number;
+  userName: string;
+  password: string;
+}
+
+interface SigninData {
+  userName: string;
+  password: string;
+}
 
 export const getMatchingShows = async (input: string): Promise<Show[]> => {
   const response: AxiosResponse<Show[]> = await axios.get(
@@ -35,4 +49,42 @@ export const getAllSubscriptionPrices = async (): Promise<SubscriptionPlan[]> =>
   );
   const subscriptionPrices = response.data;
   return subscriptionPrices;
+};
+
+export const signUserUp = async (signupData: SignupData): Promise<User> => {
+  const response: AxiosResponse<User> = await axios.post(
+    `${SERVER_HOST}/users/signup`,
+    signupData,
+    { withCredentials: true }
+  );
+  const user = response.data;
+  return user;
+};
+
+export const signUserIn = async (signinData: SigninData): Promise<User> => {
+  const response: AxiosResponse<User> = await axios.post(
+    `${SERVER_HOST}/users/signin`,
+    signinData,
+    { withCredentials: true }
+  );
+  const user = response.data;
+  return user;
+};
+
+export const getCurrentUser = async (): Promise<User> => {
+  const response: AxiosResponse<User> = await axios.get(`${SERVER_HOST}/users/current-user`, {
+    withCredentials: true,
+  });
+  const user = response.data;
+  return user;
+};
+
+export const signUserOut = async (): Promise<void> => {
+  await axios.post(
+    `${SERVER_HOST}/users/signout`,
+    {},
+    {
+      withCredentials: true,
+    }
+  );
 };
---
+++ client/common/interfaces/user.ts
@@ -0,0 +1,9 @@
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  countryId: number;
+  userName: string;
+}
+
+export default User;
