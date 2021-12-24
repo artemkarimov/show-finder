a2a514b5f5f1587cf9c7461781c45f01eb67c93e
artemkarimov <tymwrk1@gmail.com>
2021-12-24 23:59:31 +0200

    Client: add remove option to comment item

--- client/components/comment-item/index.tsx
+++ client/components/comment-item/index.tsx
@@ -1,6 +1,9 @@
 import type { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+import ClearIcon from '@mui/icons-material/Clear';
 
 import styles from './styles.module.scss';
+import { State } from '../../store';
 
 interface Props {
   user: string;
@@ -8,11 +11,20 @@ interface Props {
 }
 
 const CommentItem: FunctionComponent<Props> = ({ user, text }) => {
+  const currentUser = useSelector((state: State) => state.auth.currentUser);
+  const canBeRemoved = user === currentUser?.userName;
   return (
     <div className={styles.comment}>
-      <p>
-        <span>{user}</span> left the comment:
-      </p>
+      <div>
+        <p>
+          <span>{user}</span> left the comment:
+        </p>
+        {canBeRemoved && (
+          <div className={styles['delete-icon']}>
+            <ClearIcon />
+          </div>
+        )}
+      </div>
       <p className={styles.text}>{text}</p>
     </div>
   );
