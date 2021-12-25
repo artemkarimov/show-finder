e8b86ffb3111e457a17adad2f73324478bb09fe4
artemkarimov <tymwrk1@gmail.com>
2021-12-25 04:50:30 +0200

    Client: add dialog component to header component

--- client/components/header/index.tsx
+++ client/components/header/index.tsx
@@ -1,53 +1,73 @@
 import type { FunctionComponent } from 'react';
 import Link from 'next/link';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PersonIcon from '@mui/icons-material/Person';
 
 import { State } from '../../store';
+import Dialog from '../dialog';
+import Button from '../buttons/button';
 import StaticRoutes from '../../common/enums/static-routes';
 import { signUserOut } from '../../api/api-helper';
 import styles from './styles.module.scss';
 import { authActions } from '../../store/slices/auth-slice';
 
 const Header: FunctionComponent = () => {
+  const [confirmDialogOpened, setConfirmDialogOpened] = useState<boolean>(false);
   const isAuthenticated = useSelector((state: State) => state.auth.isAuthenticated);
   const currentUser = useSelector((state: State) => state.auth.currentUser);
   const dispatch = useDispatch();
   const signoutHandler = async () => {
     await signUserOut();
+    setConfirmDialogOpened(false);
     dispatch(authActions.signOut());
   };
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <Link href={StaticRoutes.HOME}>ShowFinder</Link>
-      </div>
-      <nav>
-        <ul>
-          {!isAuthenticated && (
-            <>
-              <li>
-                <Link href={StaticRoutes.SIGNIN}>Sign in</Link>
-              </li>
-              <li>
-                <Link href={StaticRoutes.SIGNUP}>Sign up</Link>
-              </li>
-            </>
-          )}
-          {isAuthenticated && (
-            <>
-              <li className={styles['current-user']}>
-                <PersonIcon htmlColor="white" />
-                <p>{currentUser?.userName}</p>
-              </li>
-              <li style={{ marginLeft: '3rem' }} onClick={signoutHandler}>
-                Sign out
-              </li>
-            </>
-          )}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <Link href={StaticRoutes.HOME}>ShowFinder</Link>
+        </div>
+        <nav>
+          <ul>
+            {!isAuthenticated && (
+              <>
+                <li>
+                  <Link href={StaticRoutes.SIGNIN}>Sign in</Link>
+                </li>
+                <li>
+                  <Link href={StaticRoutes.SIGNUP}>Sign up</Link>
+                </li>
+              </>
+            )}
+            {isAuthenticated && (
+              <>
+                <li className={styles['current-user']}>
+                  <PersonIcon htmlColor="white" />
+                  <p>{currentUser?.userName}</p>
+                </li>
+                <li style={{ marginLeft: '3rem' }} onClick={() => setConfirmDialogOpened(true)}>
+                  Sign out
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </header>
+      {confirmDialogOpened && (
+        <Dialog title="Confirm Logging Out">
+          <p>You will be logged out</p>
+          <div>
+            <Button flat={true} clickHandler={() => setConfirmDialogOpened(false)}>
+              Cancel
+            </Button>
+            <Button flat={false} clickHandler={signoutHandler}>
+              Okay
+            </Button>
+          </div>
+        </Dialog>
+      )}
+    </>
   );
 };
 
